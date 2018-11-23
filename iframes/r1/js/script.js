671dcd67854ba2bdd4e297a395b256957836f8ce
Jose Castillo <k.y.258@hotmail.com>
2018-11-23 17:28:28 -0300

    UPDATE LAST

--- iframes/r1/js/script.js
+++ iframes/r1/js/script.js
@@ -89,110 +89,37 @@ var newheight =  parseInt(currheight)  + amount;
   }
 ///TOUCH ZOOMvar move_event = 'mousemove';
 
-var move_event = 'mousemove';
-
-var up_event = 'mouseup';
-
-var down_event = 'mousedown';
 
 
 
 
-if ("ontouchstart" in window) {
+window.addEventListener("touchstart", ventana(event));
+
+
+function ventana(event) {
 move_event = 'touchmove';
 up_event = 'touchend';
 down_event = 'touchstart';
+
+
+alert(event.type)
 }
 
 
 
-
-window.addEventListener("touchstart", ventana(e));
-
-function ventana(e) {
-x=event.clientX;
-y=event.clientY;
-}
+window.addEventListener('touchmove', function(e) {
 
 
-window.('ontouchstart')
-rxStart = "ontouchstart" in window  e.targetTouches[0].pageX : e.screenX;
-ryStart = "ontouchstart" in window ? e.targetTouches[0].pageY : e.screenY;
+    console.log("touchpoint["0"].pageX = " + e.changedTouches[0].pageX);
+    console.log("touchpoint["0"].pageY = " + e.changedTouches[0].pageY);
+
+
+} 
 
 
 
 
-if (e.touches.length == 2) {
 
-var xStart = "ontouchstart" in window ? e.targetTouches[1].pageX : 0;
-
-var yStart = "ontouchstart" in window ? e.targetTouches[1].pageY : 0;
-
-}
-
-var prevDiff = -1;
-var scaling = false;
-
-//Con touchstar
-if ("ontouchstart" in window) {
-//var touches = e.changedTouches;
-if (e.touches.length != 2) {
-//Aquí hacemos lo que se tenga que hacer cuando se usa un dedo
-}
-
-if (e.touches.length == 2) {
-
-scaling = true;
-
-alert("DOS DEDOS");
-
-}
-
-}
-
-//Con touchmove
-if(scaling) {
-// Calculate the distance between the two pointers
-
-var curDiff = Math.abs(ev.targetTouches[0].pageX - ev.targetTouches[1].pageX);
-
-if (prevDiff > 0) {
-
-if (curDiff > prevDiff) {
-
-// The distance between the two pointers has decreased
-
-zoomMas();
-
-}
-
-if (curDiff < prevDiff) {
-
-// The distance between the two pointers has increased
-
-zoomMenos();
-
-}
-
-}
-
-// Cache the distance for the next move event
-
-prevDiff = curDiff;
-
-}
-
-//Con touchend
-
-if(scaling) {
-
-zoomEnd(e);
-
-prevDiff = -1;
-
-scaling = false;
-
-}
 
 
 
